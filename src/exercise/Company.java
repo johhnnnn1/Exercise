2f019500e2bd30b51633113494090f4be4d8b6e7
johhnnnn1 <balauca.ionut@gmail.com>
2022-10-25 14:13:11 +0300

    first commit

---
+++ src/exercise/Company.java
@@ -0,0 +1,18 @@
+package exercise; 
+
+public class Company
+{
+    private String name;
+    public Company(String name)
+    {
+        this.name=name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
